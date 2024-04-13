fabdc7634e7732082af55f3920d044203084c914
Saurabh209 <saurabhhh2613@gmail.com>
2024-04-13 14:44:02 +0530

    new area discover

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Variable Declaration 
 const mainBody = document.querySelector("main");
 const navbar = document.querySelector(".navbar");
 const userField = document.querySelector(".navbar-search");
@@ -12,6 +13,14 @@ const heading = document.querySelector("h1");
 let username;
 let userProfile;
 let aboutSection;
+let nameHolder;
+let loginNameContainer;
+let bioContainerValue;
+let followers;
+let following;
+
+
+// Main code
 
 function main() {
   const newBtn = document.createElement("button");
@@ -20,12 +29,16 @@ function main() {
   newBtn.innerText = "Clear";
   userField.appendChild(newBtn);
 
+  //button click area
   newBtn.addEventListener("click", () => {
     window.location.reload();
   });
-
+  
+  // Main Container
   mainContainer.classList.add("ContainerVisible");
   mainContainer.style.boxShadow='rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset '; 
+  
+  // left Area
   const leftArea = document.createElement("div");
   leftArea.classList.add("leftArea");
   mainContainer.appendChild(leftArea);
@@ -33,6 +46,50 @@ function main() {
   userProfile = document.createElement("div");
   userProfile.classList.add("userProfile");
   leftArea.appendChild(userProfile);
+
+  const nameContainer = document.createElement("div");
+  nameContainer.classList.add('nameContainer');
+  leftArea.appendChild(nameContainer);
+
+  nameHolder = document.createElement('div');
+  nameHolder.classList.add("nameHolder");
+  nameContainer.append(nameHolder);
+
+ 
+
+  loginNameContainer = document.createElement('div');
+  loginNameContainer.classList.add("loginNameContainer");
+  nameContainer.append(loginNameContainer);
+
+  bioContainer = document.createElement('div');
+  bioContainer.classList.add('bioContainer');
+  leftArea.appendChild(bioContainer);
+
+  bioContainerValue = document.createElement('div');
+  bioContainerValue.classList.add('bioContainerValue');
+  bioContainer.appendChild(bioContainerValue);
+
+ 
+  //Followers section
+  const followContainer = document.createElement('div');
+  followContainer.classList.add('followContainer');
+  bioContainerValue.appendChild(followContainer);
+
+
+  followers = document.createElement('div');
+  followers.classList.add('followers');
+
+  const line = document.createElement('hr');
+  line.classList.add('horizontalLine');
+  
+
+  following = document.createElement('div');
+  following.classList.add('following');
+  followContainer.appendChild(followers);
+  followContainer.appendChild(line);
+  followContainer.appendChild(following);
+
+  //Followers section end
 
   const rightArea = document.createElement("div");
   rightArea.classList.add("rightArea");
@@ -51,6 +108,13 @@ function main() {
       image.src = responseData.avatar_url;
       image.classList.add("imageConfig");
       userProfile.appendChild(image);
+
+      nameHolder.innerText=`${responseData.name}`;
+
+      loginNameContainer.innerText=`(${responseData.login})`;
+
+      followers.innerText=`Followers:${responseData.followers}`;
+      following.innerText=`Following:${responseData.following}`;
     })
     .catch((error) => {
       console.log(error);
